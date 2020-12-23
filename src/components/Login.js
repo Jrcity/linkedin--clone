8bd56e1123a96459e6477cd8a84893a31b961ae3
Jrcity <redemptionjonathan1@gmail.com>
2020-12-23 17:08:21 +0100

    login component done, implementing login logic configs to work with google auths, email & password and signup as a new user

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,13 +6,22 @@ import {
   Divider,
   Grid,
 } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import illustration from '../assets/referral.svg';
-import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [signupEmail, setSignupEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signupPassword, setSignupPassword] = useState('');
+  const [fullname, setFullname] = useState('');
+
+  const handleLogin = () => {};
+  const handleSignup = () => {};
+  const signinWithGoogle = () => {};
+
   return (
     <div className={'login'}>
       <div className={'login__header'}>
@@ -28,23 +37,28 @@ function Login() {
       </div>
       <div className={'login__body'}>
         <Grid container>
-          <Grid item xs={12} md={6}>
+          <Grid tem xs={12} md={6}>
             <h1 className={'login__text'}>
-              Welcome to your professional community
+              Welcome to your professional community.
             </h1>
             <form className={'login__details'}>
               <input
                 className={'login__input'}
-                id={'sign'}
                 type={'email'}
                 placeholder={'E-mail Address'}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <input
                 className={'login__input'}
                 type={'password'}
                 placeholder={'Password'}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
-              <Button className={'login__button'}>Sign In</Button>
+              <Button onClick={handleLogin} className={'login__button'}>
+                Sign In
+              </Button>
               <div className={'login__divider'}>
                 <Divider />
               </div>
@@ -53,20 +67,32 @@ function Login() {
               <div className={'login__option'}>
                 <Button>Signin with Google</Button>
               </div>
-              <Accordion>
-                <AccordionSummary>Not a member? Signup.</AccordionSummary>
+              <Accordion className={'login__details signup__details'}>
+                <AccordionSummary>
+                  Not a member? Click to<strong> signup.</strong>
+                </AccordionSummary>
                 <AccordionDetails>
-                  <form classname={'login__details'}>
+                  <form>
                     <input
                       className={'login__input'}
-                      id={'sign'}
+                      type={'text'}
+                      value={fullname}
+                      onChange={(e) => setFullname(e.target.value)}
+                      placeholder={'Full Name'}
+                    />
+                    <input
+                      className={'login__input'}
                       type={'email'}
                       placeholder={'E-mail Address'}
+                      value={signupEmail}
+                      onChange={(e) => setSignupEmail(e.target.value)}
                     />
                     <input
                       className={'login__input'}
                       type={'password'}
                       placeholder={'Password'}
+                      value={signupPassword}
+                      onChange={(e) => setSignupPassword(e.target.value)}
                     />
                     <Button className={'login__button'}>Sign Up</Button>
                   </form>
@@ -74,7 +100,18 @@ function Login() {
               </Accordion>
             </div>
           </Grid>
-          <Grid item xs={12} onPointerDown md={6}>
+          <Grid
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+            }}
+            item
+            xs={12}
+            onPointerDown
+            md={6}
+          >
             <img
               className={'illustration'}
               src={illustration}
